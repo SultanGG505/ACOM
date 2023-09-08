66e0d2f3ad2466935f98dfe6979baf044be40e1a
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-09-08 20:22:32 +0300

    smth
    проверил вебку на ноутбуке для второй задачи

--- LR_1/TASK_2.py
+++ LR_1/TASK_2.py
@@ -1,8 +1,8 @@
 import cv2
 
-cap = cv2.VideoCapture(r'..\media\lol.mp4')
+# cap = cv2.VideoCapture(r'..\media\lol.mp4')
 # если указать 0, то будет вебка
-# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 
 # cv2.CAP_PROP_BRIGHTNESS - яркость (0-1)
@@ -13,6 +13,8 @@ cap = cv2.VideoCapture(r'..\media\lol.mp4')
 cap.set(cv2.CAP_PROP_BRIGHTNESS, 0.5)
 cap.set(cv2.CAP_PROP_CONTRAST, 0.8)
 
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 100)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 100)
 while True:
     # Захват кадра из видеопотока.
     ret, frame = cap.read()
@@ -22,9 +24,10 @@ while True:
         break
 
     # Отображение кадра на экране.
-    resize = cv2.resize(frame, (640, 360))  # изменение расширения
-    cv2.normalize(frame, frame, 0, 277, cv2.NORM_MINMAX)
-    cv2.imshow("Video", resize)
+    # resize = cv2.resize(frame, (640, 360))
+    # изменение расширения
+    # cv2.normalize(frame, frame, 0, 277, cv2.NORM_MINMAX)
+    cv2.imshow("Video", frame) #если mp4, то поменять frame на resize
 
     # Выход на q.
     if cv2.waitKey(25) & 0xFF == ord('q'):
