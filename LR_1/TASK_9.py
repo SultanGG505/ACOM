75cff77e16ce89c76c4865ee5c22bb01ebf81eaf
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-09-08 22:27:32 +0300

    TASK_9
    сделал последнюю задачу с телефоном, подключаю через DROID_CAM, теперь осталось сделать отчёт.

--- LR_1/TASK_9.py
+++ LR_1/TASK_9.py
@@ -1,7 +1,4 @@
-# Задание 3 Отобразить видео в окне. Рассмотреть методы класса
-# VideoCapture и попробовать отображать видео в разных форматах, в частности
-# размеры и цветовая гамма.
-
+# Задание 9 подключить телефон
 import cv2
 
 # cap = cv2.VideoCapture(r'..\media\lol.mp4')
@@ -14,11 +11,6 @@ cap = cv2.VideoCapture(1)
 # cv2.CAP_PROP_SATURATION - насыщенность (0-1)
 # cv2.CAP_PROP_HUE - оттенок (0-1)
 
-cap.set(cv2.CAP_PROP_BRIGHTNESS, 0.5)
-cap.set(cv2.CAP_PROP_CONTRAST, 0.8)
-
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 300)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
 while True:
     # Захват кадра из видеопотока.
     ret, frame = cap.read()
