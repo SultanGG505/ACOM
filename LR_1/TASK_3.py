c54896e7d6c6bf2f2ff43fa9e9568fe6e7759044
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-09-08 20:48:01 +0300

    TASK_5
    переделал 5ое задание, теперь оно полностью удовлетворяет условию

--- LR_1/TASK_3.py
+++ LR_1/TASK_3.py
@@ -42,22 +42,3 @@ cap.release()
 cv2.destroyAllWindows()
 
 
-
-# cap = cv2.VideoCapture(0)
-# w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-# fourcc = cv2.VideoWriter_fourcc(*'XVID')
-# video_writer = cv2.VideoWriter("output2.mp4", fourcc, 25, (w, h))
-# while True:
-#     ret, img = cap.read()
-#     cv2.imshow("camera", img)
-#     if cv2.waitKey(25) == ord('q'): # Клавиша Esc
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
-
-
-# hsv = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
-# cv2.namedWindow("Display window",cv2.WINDOW_KEEPRATIO)
-# cv2.imshow("Display window", hsv)
-# cv2.waitKey(0)
