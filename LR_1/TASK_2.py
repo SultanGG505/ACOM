082873a0648cb5c2c8a091f2c7da0905ac9c624b
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-09-08 20:28:31 +0300

    smth
    переименовал файлы согласно делению на номера задач и удалил лишний кусок кода

--- LR_1/TASK_2.py
+++ LR_1/TASK_2.py
@@ -1,85 +1,35 @@
+# Задание 2 Вывести на экран изображение. Протестировать три
+# возможных расширения, три различных флага для создания окна и три
+# различных флага для чтения изображения.
+
+# Примеры флагов для создания окна
+# cv2.WINDOW_NORMAL - Окно с изменяемым размером
+# cv2.WINDOW_AUTOSIZE - Окно с фиксированным размером
+# cv2.WINDOW_FULLSCREEN - Полноэкранное окно
+
+# Примеры флагов для чтения изображения
+# cv2.IMREAD_COLOR - Чтение изображения в цвете (по умолчанию)
+# cv2.IMREAD_GRAYSCALE - Чтение изображения в оттенках серого
+# cv2.IMREAD_UNCHANGED - Чтение изображения без изменений
+
 import cv2
+# Загружаем изображение с разными расширениями
+image_extensions = ["png", "jpg", "bmp"]
 
-# cap = cv2.VideoCapture(r'..\media\lol.mp4')
-# если указать 0, то будет вебка
-cap = cv2.VideoCapture(0)
+for ext in image_extensions:
+    image_path = r'..\media\1.jpg'
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Чтение в оттенках серого
 
+    if img is not None:
+        cv2.namedWindow(f"Image ({ext})", cv2.WINDOW_NORMAL)  # Окно с изменяемым размером
+        cv2.imshow(f"Image ({ext})", img)
 
-# cv2.CAP_PROP_BRIGHTNESS - яркость (0-1)
-# cv2.CAP_PROP_CONTRAST - контраст (0-1)
-# cv2.CAP_PROP_SATURATION - насыщенность (0-1)
-# cv2.CAP_PROP_HUE - оттенок (0-1)
-
-cap.set(cv2.CAP_PROP_BRIGHTNESS, 0.5)
-cap.set(cv2.CAP_PROP_CONTRAST, 0.8)
-
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 100)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 100)
-while True:
-    # Захват кадра из видеопотока.
-    ret, frame = cap.read()
-
-    if not ret:
-        print("Конец видео.")
-        break
-
-    # Отображение кадра на экране.
-    # resize = cv2.resize(frame, (640, 360))
-    # изменение расширения
-    # cv2.normalize(frame, frame, 0, 277, cv2.NORM_MINMAX)
-    cv2.imshow("Video", frame) #если mp4, то поменять frame на resize
-
-    # Выход на q.
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
-
-# Освобождение памяти
-cap.release()
-cv2.destroyAllWindows()
-
-# # создадим объект VideoCapture для захвата видео
-# cap = cv2.VideoCapture(r'C:\Users\AlmaZ\Videos\papka\11.mp4', cv2.CAP_ANY)
-#
-# # Если не удалось открыть файл, выводим сообщение об ошибке
-# if cap.isOpened() == False:
-#     print('Не возможно открыть файл')
-#
-# # Пока файл открыт
-# while cap.isOpened():
-#     # поочередно считываем кадры видео
-#     fl, img = cap.read()
-#     # если кадры закончились, совершаем выход
-#     if img is None:
-#         break
-#     # выводим текущий кадр на экран
-#     cv2.imshow("Cat", img)
-#     # при нажатии клавиши "q", совершаем выход
-#     if cv2.waitKey(25) == ord('q'):
-#         break
-#
-# # освобождаем память от переменной cap
-# cap.release()
-# # закрываем все открытые opencv окна
-# cv2.destroyAllWindows()
-
-#
+        # Ждем нажатия клавиши и закрываем окно по нажатию клавиши
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print(f"Не получилось открыть изображение в этом формате {ext}")
 
 
-# cap = cv2.VideoCapture(0)
-# w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-# fourcc = cv2.VideoWriter_fourcc(*'XVID')
-# video_writer = cv2.VideoWriter("output2.mp4", fourcc, 25, (w, h))
-# while True:
-#     ret, img = cap.read()
-#     cv2.imshow("camera", img)
-#     if cv2.waitKey(25) == ord('q'): # Клавиша Esc
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
 
 
-# hsv = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
-# cv2.namedWindow("Display window",cv2.WINDOW_KEEPRATIO)
-# cv2.imshow("Display window", hsv)
-# cv2.waitKey(0)
