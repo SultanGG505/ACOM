548d7dd948f8e42b77fef5ac96eed30d8a948728
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-09-09 10:54:52 +0300

    добавил blur в TASK_6

--- LR_1/TASK_6.py
+++ LR_1/TASK_6.py
@@ -26,7 +26,9 @@ top_left_y = (height - rect_height) // 2
 bottom_right_x = top_left_x + rect_width
 bottom_right_y = top_left_y + rect_height
 cv2.rectangle(img, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (0, 0, 255), 2)
-
+ROI = img[top_left_y:top_left_y+rect_height, top_left_x:top_left_x+rect_width]
+blur = cv2.GaussianBlur(ROI, (51,51), 0)
+img[top_left_y:top_left_y+rect_height, top_left_x:top_left_x+rect_width] = blur
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)  # Окно с изменяемым размером
 cv2.imshow('image', img)
 cv2.waitKey(0)
