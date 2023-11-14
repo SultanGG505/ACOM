780123b02358b8a47f2bf000e5137d69de314a39
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-11-14 21:00:06 +0300

    lr 2 task 1
    Задание 1 Прочитать изображение с камеры и перевести его в формат
    HSV.

---
+++ LR_2/task1.py
@@ -0,0 +1,18 @@
+# Задание 1 Прочитать изображение с камеры и перевести его в формат
+# HSV.
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    cv2.imshow('hsv_frame', hsv)
+
+    if cv2.waitKey(1) & 0xFF == ord(' '):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
