2e96f8ff2c22d67529def86920e9dab95186acde
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-11-24 10:19:19 +0300

    Update motion_vid.py
    добавил комментарии по коду

--- LR_5/motion_vid.py
+++ LR_5/motion_vid.py
@@ -3,6 +3,12 @@ import numpy as np
 
 i = 0
 
+# Здесь определяется функция main с четырьмя аргументами:
+# kernel_size (размер ядра для гауссова размытия),
+# standard_deviation (стандартное отклонение для гауссова размытия),
+# delta_tresh (порог значений для бинаризации разности между кадрами)
+# и min_area (минимальная площадь контура для рассмотрения).
+
 def main(kernel_size, standard_deviation, delta_tresh, min_area):
     global i
     i += 1
@@ -16,10 +22,17 @@ def main(kernel_size, standard_deviation, delta_tresh, min_area):
     w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    # Формируется кодек для записи видео, который соответствует формату
+    # MP4(в данном случае 'mp4v').
     video_writer = cv2.VideoWriter('result_videos/result' + str(i) + '.mp4', fourcc, 144, (w, h))
+    # Создается объект VideoWriter, который будет использовать
+    # вышеуказанный кодек для записи выводимого видео с заданной
+    # частотой кадров(144 кадров в секунду) и размерами кадров w на h.
+
 
     while True:
         # сохраняем старый кадр чтобы вычислить разниц между кадрами
+        print("...")
         old_img = img.copy()
         ok, frame = video.read()
         if not ok:
@@ -30,8 +43,13 @@ def main(kernel_size, standard_deviation, delta_tresh, min_area):
 
         # вычисляем разницу
         diff = cv2.absdiff(img, old_img)
+        # Рассчитывается абсолютная разность между cтарым и новым
+        # кадром в серых тонах для обнаружения движения.
         # бинаризируем её превращая пиксели, превышающие порог delta_tresh, в белый цвет, а остальные в черный
         # сохраняем только пороговое значение
+        # Абсолютная разность бинаризуется: пиксели с
+        # интенсивностью выше delta_tresh становятся
+        # белыми(255), остальные - черными.
         thresh = cv2.threshold(diff, delta_tresh, 255, cv2.THRESH_BINARY)[1]
         # находим контуры
         (contors, hierarchy) = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -45,6 +63,7 @@ def main(kernel_size, standard_deviation, delta_tresh, min_area):
 
     video_writer.release()
 
+    print("готово!")
 
 kernel_size = 3
 standard_deviation = 50
