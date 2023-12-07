ad934a5a4a5f9cfe3715d0f2111bebc87d22eeab
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-12-07 19:31:32 +0300

    подсчёт процента корректной работы

--- LR_6/Multilayer_Perceptron_MNIST.py
+++ LR_6/Multilayer_Perceptron_MNIST.py
@@ -5,9 +5,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop
 import time
-
 start_time = time.time()
-
 # загрузка данных MNIST (первый кортеж - тренировочные изображения и метки, а второй - тестовые изображения и метки)
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 # преобразование каждого изображения (тренировочных и тестовых) в одномерный массив
@@ -19,13 +17,11 @@ x_test = x_test.astype('float32')
 # нормализация значений пикселей изображений в диапазоне от 0 до 1
 x_train /= 255
 x_test /= 255
-
 # в базе данных MNIST содержится 10 классов цифр от 0 до 9
 num_classes = 10
 # преобразование меток в категории для каждого типа данных
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-
 # создание новой модели многослойного персептрона
 model = Sequential()
 # добавление первого слоя
@@ -37,7 +33,6 @@ model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.2))
 # добавление выходного слоя
 model.add(Dense(num_classes, activation='softmax'))
-
 # Компиляция модели и обучение ее на тренировочных данных
 # настройка процесса обучения модели
 model.compile(loss='categorical_crossentropy',
@@ -50,10 +45,18 @@ history = model.fit(x_train, y_train,
                     verbose=1, # вывод информации о потерях, метриках и прогресс-бар
                     validation_data=(x_test, y_test)) # тестовые данные будут использоваться для проверки производительности модели во время обучения
 
+print('==============================================================')
 # оценка потерь и точности модели
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Потери на тестовых данных:', score[0])
 print('Точность модели на тестовых данных:', score[1])
 
+# подсчёт процента корректной работы на тестовой базе
+accuracy = score[1]
+percent_correct = accuracy * 100
+print('Процент корректной работы:', percent_correct)
+
+# подсчёт затраченного времени
 end_time = time.time()
 print('Затраченное время:', end_time - start_time)
+print('==============================================================')
