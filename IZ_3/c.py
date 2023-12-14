d6c609961c7de97a25e8624bfe186892a03ac9b6
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-12-14 20:01:02 +0300

    бета версия редактора

--- IZ_3/c.py
+++ IZ_3/c.py
@@ -1,17 +1,20 @@
-import nibabel as nib
-import matplotlib.pyplot as plt
 
-# Замените 'path/to/your/file.nii' на путь к вашему файлу NIfTI
-file_path = r'C:\Users\User\Documents\GitHub\ACOM\IZ_3\parsed_data\bad\1.2.643.5.1.13.13.12.2.77.8252.00000312101100040009080805131200.nii'
+import subprocess
+import os
 
-# Загрузка файла NIfTI
-img = nib.load(file_path)
+def open_nii_file_in_mricrogl(file_path, mricrogl_path):
+    if not os.path.isfile(file_path):
+        print(f"Файл {file_path} не найден.")
+        return
 
-# Получение данных изображения
-data = img.get_fdata()
+    mricrogl_executable = os.path.join(mricrogl_path, 'MRIcroGL.exe')
 
-# Вывод среза изображения (можно настроить срез по вашему усмотрению)
-slice_number = 1
-plt.imshow(data[:, :, :], cmap='gray')
-plt.title('NIfTI Image')
-plt.show()
+    command = [mricrogl_executable, file_path, "-cm 4hot"]
+
+    subprocess.run(command)
+
+# Пример использования
+nii_file_path = r'C:\Users\User\Documents\GitHub\ACOM\IZ_3\parsed_data\bad\1.2.643.5.1.13.13.12.2.77.8252.00000312101100040009080805131200.nii'
+mricrogl_installation_path = r'C:\Users\User\Desktop\MRIcroGL_windows\MRIcroGL'  # Замените на ваш путь
+
+open_nii_file_in_mricrogl(nii_file_path, mricrogl_installation_path)
