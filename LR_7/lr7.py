c6b3989410b7ac76e21a6c1f7959899b8521ffc1
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-12-21 04:30:18 +0300

    LR_7_1

--- LR_7/lr7.py
+++
@@ -1,8 +0,0 @@
-import pytesseract
-import cv2
-
-pytesseract.pytesseract.tesseract_cmd = (r"C:\Program Files\Tesseract-OCR\tesseract.exe")
-
-img = cv2.imread('test1.png')
-text = pytesseract.image_to_string(img, lang='rus+eng')
-print(text)
