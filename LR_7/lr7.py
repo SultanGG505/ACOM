c9373f6ec21b58f8bbe54cdce07defbfbde821a8
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-12-21 03:58:40 +0300

    пример

--- LR_7/lr7.py
+++ LR_7/lr7.py
@@ -2,3 +2,7 @@ import pytesseract
 import cv2
 
 pytesseract.pytesseract.tesseract_cmd = (r"C:\Program Files\Tesseract-OCR\tesseract.exe")
+
+img = cv2.imread('test1.png')
+text = pytesseract.image_to_string(img, lang='rus+eng')
+print(text)
